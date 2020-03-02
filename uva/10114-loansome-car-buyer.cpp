18fb4abed0cc0cccfb83fdd9ff4f0d39f5d132f4
Vinicius Juliao <vrjuliao@hotmail.com>
2020-03-02 18:16:33 -0300

    UVa 10114 problem solved

--- uva/10114-loansome-car-buyer.cpp
+++ uva/10114-loansome-car-buyer.cpp
@@ -5,47 +5,45 @@ int main(){
 	int months, rounds;
 	int round_index, next_round_index, month_count;
 	float part, total, loan, next_loan, loan_total;
-	bool total_gt_loan;
+	float montly_payment;
+	bool printed;
 
 	cin >> months >> part >> total >> rounds;
 	while (months >= 0){
+		montly_payment = total / months;
 		cin >> round_index >> loan;
-		loan_total = total * (1.0 - loan);
+		loan_total = (total+part) * (1.0 - loan);
 		round_index++;
 		month_count=0;
+		printed = false;
 
 		while (rounds >= 2){
-			// cout << "here" << endl;
 			cin >> next_round_index >> next_loan;
 			for(;round_index<next_round_index && total > loan_total; round_index++){
 				month_count++;
-				total -= part;
+				total -= montly_payment;
 				loan_total *= (1.0 - loan);
-			cout << "Total: " << total << endl;
-			cout << "Total loan: " << loan_total << endl;
 			}
 			if(total < loan_total){
-			cout << "here" << endl;
-				cout << "print ";
-				cout << month_count-1 << " month";
-				if(month_count > 1) cout << 's';
+				cout << month_count << " month";
+				if(month_count != 1) cout << 's';
 				cout << endl;
+				printed = true;
 				break;
 			}
 			round_index = next_round_index;
 			loan = next_loan;
 			rounds--;
 		}
-		if(total > loan_total){
-			while(month_count < months || total > loan_total){
+		if(total > loan_total || !printed){
+			while(month_count < months && total > loan_total){
 				month_count++;
-				total -= part;
+				total -= montly_payment;
 				loan_total *= (1.0 - loan);
-				cout << "--Total: " << total << endl;
-				cout << "--Total loan: " << loan_total << endl;
 			}
-			cout << month_count-1 << " month";
-			if(month_count > 1) cout << 's';
+			cout << month_count << " month";
+			if(month_count != 1)
+				cout << 's';
 			cout << endl;
 		}
 		cin >> months >> part >> total >> rounds;
